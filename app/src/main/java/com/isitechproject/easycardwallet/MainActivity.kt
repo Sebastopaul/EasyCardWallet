eee7fbec96f31ce30c9b54e0448806bef34895c8
Sébastien Lamard <sebastien.lamard@ecole-isitech.fr>
2024-05-10 15:17:55 +0200

    refactor(main-activity): #6 remove useless example stuff from main

--- app/src/main/java/com/isitechproject/easycardwallet/MainActivity.kt
+++ app/src/main/java/com/isitechproject/easycardwallet/MainActivity.kt
@@ -3,6 +3,7 @@ package com.isitechproject.easycardwallet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,33 +15,11 @@ import com.isitechproject.easycardwallet.ui.theme.EasyCardWalletTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             EasyCardWalletTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EasyCardWalletTheme {
-        Greeting("Android")
     }
 }
